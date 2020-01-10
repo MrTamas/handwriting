cac3686a3ef635acf213ed77fb0a7f442e4a4e14
MrTamas <tamas.johancsik@gmail.com>
2020-01-10 16:47:26 +0100

    filling table with mistakes

--- sudoku/functions.py
+++ sudoku/functions.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from random import randrange
+from random import randrange, choice
 
 
 diagonal_list = [(0, 0), (0, 1), (0, 2),
@@ -33,9 +33,20 @@ def print_square(df):
 def random_number(seq1, seq2):
     num = randrange(1, 10)
     if (num in seq1) or (num in seq2):
-        return random_number(seq1, seq2)
+        num = random_number(seq1, seq2)
+    return num
+
+
+def random_number2(seq1, seq2):
+    num = randrange(1, 10)
+    if (num in seq1) or (num in seq2):
+        return num, True
     else:
-        return num
+        return num, False
+
+
+def check_contains(num, seq1, seq2):
+    return (num in seq1) or (num in seq2)
 
 
 def sequence(l):
@@ -68,7 +79,7 @@ def fill_diagonal():
 def fill_recursively(df, i, j):
     seq1 = list(df.iloc[i, :])
     seq2 = list(df.iloc[:, j])
-    num = random_number(seq1, seq2)
+    num = random_number2(seq1, seq2)
     return num
 
 
@@ -81,7 +92,56 @@ def fill_rest(df):
     return df
 
 
+def fill_square(df, dim):
+    options = [x for x in range(1, 10)]
+    for i in range(dim[0], dim[1]):
+        for j in range(dim[2], dim[3]):
+            check = True
+            op = options.copy()
+            while check:
+                seq1 = list(df.iloc[i, :])
+                seq2 = list(df.iloc[:, j])
+                num = choice(op)
+                check = check_contains(num, seq1, seq2)
+                # print(seq1, seq2, check, num, op, options)
+                op.remove(num)
+            df.iloc[i, j] = num
+            options.remove(num)
+    return df
+
+
+"""
+def random_number2(seq1, seq2, options):  # df, dim
+    num = choice(options)
+    if (num in seq1) or (num in seq2):
+        options.remove(num)
+        num = random_number2(seq1, seq2, options)
+    return num
+"""
+
+
+"""
 d = fill_diagonal()
 print_square(d)
 f = fill_rest(d)
 print_square(f)
+"""
+
+def create_sudoku(counter):
+    print(counter)
+    try:
+        a = fill_diagonal()
+        b = fill_square(a, [3, 6, 0, 3])
+        c = fill_square(b, [0, 3, 3, 6])
+        d = fill_square(c, [6, 8, 3, 6])
+        e = fill_square(d, [3, 6, 6, 8])
+        f = fill_square(e, [6, 8, 0, 3])
+        g = fill_square(f, [0, 3, 6, 8])
+        print_square(g)
+        print(counter)
+    except IndexError:
+        counter += 1
+        create_sudoku(counter)
+
+
+create_sudoku(0)
